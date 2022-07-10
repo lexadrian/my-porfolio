2fa432e898b340c78a939999ebe274e60872ff80
Lex Adrian <lexadrian6@gmail.com>
2022-07-10 16:26:24 +0800

    added code structure

---
+++ src/store/modules/sample.js
@@ -0,0 +1,23 @@
+const state = {
+  count: 0,
+};
+
+// getters
+const getters = {
+  getCount: (state) => {
+    return state.count;
+  },
+};
+
+// mutations
+const mutations = {};
+// actions
+const actions = {};
+
+export default {
+  // namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
